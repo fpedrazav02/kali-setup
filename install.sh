3fe68d8647d0b767b57ec41e37d1948425c250aa
Francisco Pedraza Vázquez <101214411+fpedrazav02@users.noreply.github.com>
2022-07-07 21:33:45 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -160,6 +160,17 @@ wget https://raw.githubusercontent.com/puneetsl/lotion/master/setup.sh
 chmod +x setup.sh
 sudo ./setup.sh web
 
+#INSTALLING VISUAL STUDIO CODE
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
+
 #KITTY INSTALATION
 clear
 echo -e "${BGreen}Kitty install...${Color_Off}"
