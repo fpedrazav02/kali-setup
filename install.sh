a4ac12855d34a105f77e016bc5ba996de0aa7d98
Francisco Pedraza Vázquez <101214411+fpedrazav02@users.noreply.github.com>
2022-07-07 21:22:08 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -150,6 +150,12 @@ sudo apt update
 
 sudo apt install brave-browser
 
+#INSTALING SPOTIFY
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+
+
 #KITTY INSTALATION
 clear
 echo -e "${BGreen}Kitty install...${Color_Off}"
